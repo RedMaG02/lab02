f80d974893e51486cbf3f36c61b8d3b9330ee14a
RedMaG02 <94241600+RedMaG02@users.noreply.github.com>
2023-04-09 21:55:51 +0300

    comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,9 +3,9 @@
 
 int main()
 {
-    string name;
+    string name; // string name
     std::cout << "Enter your name: ";
     std::cin >> name;
     std::cout << "Hello world from " << name << endl;
-    return 0;
+    return 0; // return 0
 }
